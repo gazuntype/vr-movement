7f3d0ecf4facd8d9d6f314f11d220a6e8c81b2f0
Olutade Ajiboye <mrbigheaddd@gmail.com>
2016-12-08 15:11:48 +0100

    fix: added dependency files

--- vr-movement/Assets/Scripts/WaypointSpawner.cs
+++ vr-movement/Assets/Scripts/WaypointSpawner.cs
@@ -3,11 +3,9 @@ using System.Collections;
 
 public class WaypointSpawner : MonoBehaviour {
 	NavMeshAgent agent;
-	public Transform target;
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
-		agent.SetDestination(target.position);
 	}
 	
 	// Update is called once per frame
