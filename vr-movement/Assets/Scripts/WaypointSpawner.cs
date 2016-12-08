f1b1c8ea083fb8b3d91dd1d91e5b4b710644176f
Olutade Ajiboye <mrbigheaddd@gmail.com>
2016-12-08 15:04:12 +0100

    feat: navmesh created
    A simple terrain has been created with some boxes has obstacles. A navmesh was also created and the player was made a navmesh agent to navigate the mesh.

---
+++ vr-movement/Assets/Scripts/WaypointSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointSpawner : MonoBehaviour {
+	NavMeshAgent agent;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
