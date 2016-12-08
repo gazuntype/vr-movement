7255415abae939b9980ba867304b1fbf82703f13
Olutade Ajiboye <mrbigheaddd@gmail.com>
2016-12-08 23:21:19 +0100

    feat: Started Coroutine checks
    WaypointSpawner script has been edited to start coroutine spawning of waypoints in a forward manner.

--- vr-movement/Assets/Scripts/WaypointSpawner.cs
+++ vr-movement/Assets/Scripts/WaypointSpawner.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WaypointSpawner : MonoBehaviour {
+	public Camera playerCamera;
 	NavMeshAgent agent;
+	IEnumerator coroutine;
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
+		coroutine = UpdateWayPointCreation();
+		StartCoroutine(coroutine);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	IEnumerator UpdateWayPointCreation()
+	{
+		while (true)
+		{
+			
+			yield return new WaitForSeconds(100);
+		}
 	}
 }
