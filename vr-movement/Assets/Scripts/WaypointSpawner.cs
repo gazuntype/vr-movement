6d7f4ce0fd2e901f0900ea7c0ac1b94f53ff7f24
Olutade Ajiboye <mrbigheaddd@gmail.com>
2016-12-09 00:08:30 +0100

    feat: Spawns Waypoint boxes
    Spawns random waypoint boxes in the general forward direction of the player. The user inputs the maximum waypoint distance and the minimum interval distance between waypoints. Also inputs the gameObject he/she wants to use to represent the waypoints.

--- vr-movement/Assets/Scripts/WaypointSpawner.cs
+++ vr-movement/Assets/Scripts/WaypointSpawner.cs
@@ -4,6 +4,15 @@ using System.Collections.Generic;
 
 public class WaypointSpawner : MonoBehaviour {
 	public Camera playerCamera;
+	[Tooltip("The maximum distance you want a waypoint to be created")]
+	public float maximumDistance = 10;
+
+	[Tooltip("The minimum distance between two waypoints")]
+	public float intervalDistance = 1;
+
+	[Tooltip("The GameObject to use as a waypoint")]
+	public GameObject waypointObject;
+
 	NavMeshAgent agent;
 	IEnumerator coroutine;
 	// Use this for initialization
@@ -22,7 +31,14 @@ public class WaypointSpawner : MonoBehaviour {
 	{
 		while (true)
 		{
-			
+			for (int i = 0; i < maximumDistance; i += (int)intervalDistance)
+			{
+				NavMeshHit hit;
+				Vector3 randomDirection = Quaternion.Euler(0, Random.Range(-30, 30), 0) * transform.forward;
+				Vector3 randomPoint = transform.position + (randomDirection * i);
+				NavMesh.SamplePosition(randomPoint, out hit, 2, NavMesh.AllAreas);
+				Instantiate(waypointObject, hit.position, Quaternion.identity);
+			}
 			yield return new WaitForSeconds(100);
 		}
 	}
